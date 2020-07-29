3ced75f132429096d77f9054670eaa80e8c39906
aaa <123@qq.com>
2020-07-29 14:49:27 +0800

    '代码压缩转义'

---
+++ src/index.js
@@ -0,0 +1,20 @@
+/*
+ * @Descripttion:
+ * @version:
+ * @Author: Remons
+ * @Date: 2020-07-18 18:07:57
+ * @LastEditors: Remons
+ * @LastEditTime: 2020-07-19 19:07:23
+ */
+
+let methods = require("./methods");
+console.log(methods)
+module.exports = {
+  dateFormat: methods.dateFormat,
+  deepClone: methods.deepClone,
+  removeArrRepeat: methods.removeArrRepeat,
+  arrSUM: methods.arrSUM,
+  arrFlat: methods.arrFlat,
+  arrGroup: methods.arrGroup,
+  IsPC: methods.IsPC,
+};
